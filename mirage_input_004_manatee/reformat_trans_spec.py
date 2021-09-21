f74d29b131844d1f2cd28cb5be02b57287686e77
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2021-09-20 22:08:06 -0700

    made reformat trans spec more general

--- mirage_input_004_manatee/reformat_trans_spec.py
+++ mirage_input_004_manatee/reformat_trans_spec.py
@@ -1,12 +1,33 @@
 from astropy.io import fits, ascii
 from astropy.table import Table
 import numpy as np
+import sys
 
-dat = ascii.read('ck_gto_f_wasp80b_1trans_R100.div',
-                 names=['wavel','rprs_noise','rprs_truth','err'])
+def reformat(filename):
+    dat = ascii.read(filename,
+                     names=['wavel','depth_noise','depth_truth','err'])
+    if "trans_R100" in filename:
+        modelType = 'transit'
+    elif "eclipse_R100" in filename:
+        modelType = 'eclipse'
+    else:
+        raise Exception("Unrecognized file type")
+    
+    outname = filename.replace('.div','.csv')
+    outDat = Table()
+    outDat['wave (um)'] = dat['wavel']
+    outDat['depth'] = dat['depth_truth']
+    
+    if modelType == 'transit':
+        outDat['Rp/R*'] = np.sqrt(outDat['depth'])
+    
+    outDat.write(outname)
 
-outDat = Table()
-outDat['wave (um)'] = dat['wavel']
-outDat['depth'] = dat['rprs_truth']
-outDat['Rp/R*'] = np.sqrt(outDat['depth'])
-outDat.write('ck_gto_f_wasp80b_1trans_R100.csv')
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        filename = 'ck_gto_f_wasp80b_1trans_R100.div'
+    else:
+        filename = sys.argv[1]
+    reformat(filename)
+    
